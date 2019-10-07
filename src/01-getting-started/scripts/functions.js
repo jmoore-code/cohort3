68a9fb5314023848370ed3d351ac50a1f5bfecda
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-07 16:43:08 -0600

    class work return negative

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -2,8 +2,10 @@
 const functions = {
     
     size: (num) => {
+        if (num < 0)  return "negative";
         if (num < 10) return "small";
         if (num < 20) return "medium";
+        
         return "large";
     },
 
