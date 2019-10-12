51193b59794e69afcca9accf9634fb8c5d5ec7df
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-12 12:22:30 -0600

    syntax update

--- src/javascript/syntax.js
+++ src/javascript/syntax.js
@@ -34,51 +34,116 @@ const functions = {
   // undefined
 
   undefined1: () => {
-      const x = undefined
-      
+    const x = undefined;
   },
 
   // sample if / else ------------------------------------
 
-  conditional1: (time) => {
-      if (time < 10) {
-          return "Good morning";
-      } else if (time < 18) {
-          return "Good afternoon";
-      } else {
-          return "Good evening";
-      }
+  conditional1: time => {
+    if (time < 10) {
+      return "Good morning";
+    } else if (time < 18) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
   },
 
   // functions ------------------------------------------
 
   // parameters
-
-  multiple1: (a,b) => {
-    return a*b;
-  }
-
   // returns
+
+  multiple1: (a, b) => {
+    return a * b;
+  },
 
   // arrays ---------------------------------------------
 
+
+
   // add to the front
+  arrayTestFront: ["Jason", "Katie"],
+  addToFront: (name) => {
+    functions.arrayTestFront.unshift(name);
+    return functions.arrayTestFront[0];
+  },
 
   // add to the end
+  arrayTestEnd: ["Jason", "Katie"],
+  addToEnd: (name) => {
+    functions.arrayTestEnd.push(name);
+    return functions.arrayTestEnd[functions.arrayTestEnd.length -1];
+  },
 
   // update values
+  arrayTestUpdate: ["Jason", "Katie"],
+  updateValue: (name) => {
+      functions.arrayTestUpdate[0] = name
+      return functions.arrayTestUpdate[0]
+  },
 
   // loops ----------------------------------------------
 
   // for
+  myForLoop: (p1) => {
+      let i;
+      let sum = 0;
+      for ( i = 0; i < 5; i++) {
+        sum = sum + i;          
+      }
+      return sum 
+  } ,
 
   // for/in
 
+  forInObj: {fname:"John", lname:"Doe", age:25},
+  myForInLoop: () => {
+      let txt = "";
+      let x;
+      for (x in functions.forInObj) {
+          (txt += functions.forInObj[x] + " ");   
+      }
+    return txt;  
+  },
+
   // while
+
+  whileArray: ["BMW", "Volvo", "Saab", "Ford"],
+  myWhileLoop: () => {
+    let txt = "";
+    let i = 0;
+    while (i < functions.whileArray.length) {
+      txt += functions.whileArray[i] + " ";
+      i++;
+    }
+    return txt;
+  },
 
   // do while
 
+  myDoWhileLoop: ()=> {
+    let i = 0;
+    let sum = 0;
+    do {sum = sum + i;
+        i++;
+      }
+     while(i<5);
+     return sum;
+    
+  },
+
   // forEach (with array and function)
+
+  myForEach: () => {
+    let sum = 0;
+    let numbers = [65, 44, 12, 4];
+    numbers.forEach(myFunction);
+    function myFunction(item) {
+      sum += item;
+    }
+    return sum;
+  }
 
   // Objects / Dictionaries ----------------------------
 
--- src/javascript/syntax.test.js
+++ src/javascript/syntax.test.js
@@ -5,14 +5,12 @@ import functions from "./syntax";
 
 test("Is this a number?", () => {
   expect(functions.num1).toBe(5);
-  console.log("true");
 });
 
 // string
 
 test("Is this a string?", () => {
   expect(functions.string1).toBe("Hello World");
-  console.log("true");
 });
 
 // boolean
@@ -54,33 +52,65 @@ test("test if/else", () => {
 // functions ------------------------------------------
 
 // parameters
+// returns
 
 test("test multiple function", ()=> {
     expect(functions.multiple1(2,2)).toBe(4);
+    expect(functions.multiple1(3,3)).toBe(9);
 
 });
 
-// returns
+
 
 // arrays ---------------------------------------------
 
 // add to the front
 
+test("test array add to front", () => {
+    expect(functions.addToFront("Bob")).toBe(functions.arrayTestFront[0]);
+});
+
 // add to the end
 
+test("test array add to end", () => {
+    expect(functions.addToEnd("Tim")).toBe(functions.arrayTestEnd[functions.arrayTestEnd.length -1]);
+});
+
 // update values
+
+test("test update value", () => {
+    expect(functions.updateValue("Larry")).toBe("Larry");
+});
 
 // loops ----------------------------------------------
 
 // for
+test("test for loop", () => {
+    expect(functions.myForLoop(5)).toBe(10);
+});
 
 // for/in
 
+test("test for in loop", () => {
+    expect(functions.myForInLoop()).toBe("John Doe 25 ");
+});
+
 // while
 
+test("test while loop", () => {
+  expect(functions.myWhileLoop()).toBe("BMW Volvo Saab Ford ");
+});
 // do while
 
+test("test do while loop", () => {
+  expect(functions.myDoWhileLoop()).toBe(10);
+});
+
 // forEach (with array and function)
+
+test("test forEach", () => {
+  expect(functions.myForEach()).toBe(125);
+});
 
 // Objects / Dictionaries ----------------------------
 
