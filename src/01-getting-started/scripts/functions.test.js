c602bac242a9c7880c6fb7da635cafdc60a107c2
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-12 21:06:50 -0600

    calculator and tax calculator update

--- src/01-getting-started/scripts/functions.test.js
+++ src/01-getting-started/scripts/functions.test.js
@@ -13,3 +13,36 @@ test('Does that add function work?', () => {
     expect(functions.add(1,2)).toBe(3);
     expect(functions.add(101,202)).toBe(303);
 });
+
+test("Does the subtract function work?", () => {
+    expect(functions.subtract(3,2)).toBe(1);
+    expect(functions.subtract(1,1)).toBe(0);
+    expect(functions.subtract(-1,1)).toBe(-2);
+    expect(functions.subtract(-1,-1)).toBe(0);
+});
+
+test("Does the multiply function work?", () => {
+    expect(functions.multiply(2,2)).toBe(4);
+    expect(functions.multiply(1,0)).toBe(0);
+    expect(functions.multiply(-2,2)).toBe(-4);
+    expect(functions.multiply(100,2)).toBe(200);
+});
+
+test("Does the divide function work?", () => {
+    expect(functions.divide(4,2)).toBe(2);
+    expect(functions.divide(1,0)).toBe(Infinity);
+    expect(functions.divide(-4,2)).toBe(-2);
+    expect(functions.divide(20,100)).toBe(0.2);
+});
+
+test("Does the tax calculator work?", () => {
+    expect(functions.taxCalculator(2)).toBe(.30);
+    expect(functions.taxCalculator(47630)).toBe(7144.5);
+    expect(functions.taxCalculator(50000)).toBe(7630.35);
+    expect(functions.taxCalculator(95259)).toBe(16908.445);
+    expect(functions.taxCalculator(100000)).toBe(18141.105);
+    expect(functions.taxCalculator(147667)).toBe(30534.525);
+    expect(functions.taxCalculator(150000)).toBe(31211.095);
+    expect(functions.taxCalculator(210371)).toBe(48718.685);
+    expect(functions.taxCalculator(250000)).toBe(61796.255);
+});
--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -8,3 +8,25 @@ import functions from './functions.js';
 idNumber.addEventListener('change', (() => {
     idNumberSize.textContent = functions.size(idNumber.value);
 }));
+
+// ********Calculator listeners
+idAddButton.addEventListener('click', (() => {
+    idResult.textContent = functions.add(Number(idCalcInput1.value), Number(idCalcInput2.value));
+}));
+
+idMinusButton.addEventListener('click', (() => {
+    idResult.textContent = functions.subtract(Number(idCalcInput1.value), Number(idCalcInput2.value));
+}));
+
+idMultiButton.addEventListener('click', (() => {
+    idResult.textContent = functions.multiply(Number(idCalcInput1.value), Number(idCalcInput2.value));
+}));
+
+idDivideButton.addEventListener('click', (() => {
+    idResult.textContent = functions.divide(Number(idCalcInput1.value), Number(idCalcInput2.value));
+}));
+
+// ********Tax calculator listeners
+idTaxButton.addEventListener('click', (() => {
+    idTaxResult.textContent = functions.taxCalculator(Number(idTaxInput.value));
+}));
