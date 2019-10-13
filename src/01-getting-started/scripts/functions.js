c404eb366249e38d97438ddc31da901e941d5b1b
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-13 11:55:00 -0600

    finished matching array functions with html buttons

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,80 +1,84 @@
-
 const functions = {
-    
-    size: (num) => {
-        if (num < 0)  return "negative";
-        if (num < 10) return "small";
-        if (num < 20) return "medium";
-        
-        return "large";
-    },
+  size: num => {
+    if (num < 0) return "negative";
+    if (num < 10) return "small";
+    if (num < 20) return "medium";
 
-    // Basic Calculator functions
+    return "large";
+  },
 
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
+  // Basic Calculator functions
 
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    },
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
 
-    multiply: (num1, num2) => {
-        return num1 * num2;       
-    },
+  subtract: (num1, num2) => {
+    return num1 - num2;
+  },
 
-    divide: (num1, num2) => {
-        return num1 / num2;
-    },
+  multiply: (num1, num2) => {
+    return num1 * num2;
+  },
 
-    // Tax Calculator Function
+  divide: (num1, num2) => {
+    return num1 / num2;
+  },
 
-    taxCalculator: (num1) => {
-        const teir1 = 47630
-        const teir2 = 47629
-        const teir3 = 52408
-        const teir4 = 62704
-               
-        if (num1 <= 47630) {
-            return num1 * 0.15
-        } else if (num1 <= 95259) { 
-            return ((num1 - 47630) * 0.205) + (teir1 *0.15);
-        } else if (num1 <= 147667) {
-            return ((num1 - 95259) * 0.26) + (teir2 * 0.205) + (teir1 * 0.15);
-        } else if (num1 <= 210371) {
-            return ((num1 - 147667) * 0.29) + (teir3 * 0.26) + (teir2 * 0.205) + (teir1 * 0.15);
-        }else {
-            return ((num1 - 210371) * 0.33) + (teir4 * 0.29) + (teir3 * 0.26) + (teir2 * 0.205) + (teir1 * 0.15);
-        }
-    },
+  // Tax Calculator Function
 
-    // Working with Arrays functions
-    myArray: [],
+  taxCalculator: num1 => {
+    const teir1 = 47630;
+    const teir2 = 47629;
+    const teir3 = 52408;
+    const teir4 = 62704;
 
-    addToArray: (num1) => { 
-        if (typeof(num1) == "number") {
-            functions.myArray.push(num1);
-            return `The number ${num1} has been added to the array`
-        } else {
-            return "Your input is not a valid number"
-        }
-    },
-
-    showArrayState: () => {
-        return functions.myArray.toString();
-    },
-
-    totalArray: () => {
-        let x = functions.myArray.reduce((accumulator, y) => accumulator + y, 0);
-        return x
-    },
-
-    clearArray: () => {
-        functions.myArray.length = 0;
-        return functions.myArray.length;
+    if (num1 <= 47630) {
+      return num1 * 0.15;
+    } else if (num1 <= 95259) {
+      return (num1 - 47630) * 0.205 + teir1 * 0.15;
+    } else if (num1 <= 147667) {
+      return (num1 - 95259) * 0.26 + teir2 * 0.205 + teir1 * 0.15;
+    } else if (num1 <= 210371) {
+      return (
+        (num1 - 147667) * 0.29 + teir3 * 0.26 + teir2 * 0.205 + teir1 * 0.15
+      );
+    } else {
+      return (
+        (num1 - 210371) * 0.33 +
+        teir4 * 0.29 +
+        teir3 * 0.26 +
+        teir2 * 0.205 +
+        teir1 * 0.15
+      );
     }
-    
+  },
+
+  // Working with Arrays functions
+  myArray: [],
+
+  addToArray: num1 => {
+    if (typeof num1 == "number") {
+      functions.myArray.push(num1);
+      return `The number ${num1} has been added to the array`;
+    } else {
+      return "Your input is not a valid number";
+    }
+  },
+
+  showArrayState: () => {
+    return functions.myArray.toString();
+  },
+
+  totalArray: () => {
+    let x = functions.myArray.reduce((accumulator, y) => accumulator + y, 0);
+    return x;
+  },
+
+  clearArray: () => {
+    functions.myArray.length = 0;
+    return functions.myArray.length;
+  }
 };
 
 export default functions;
-
--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -1,37 +1,68 @@
-import functions from './functions.js';
+import functions from "./functions.js";
 
 // **********
 //
 // Add the event listeners
-// 
+//
 
-idNumber.addEventListener('change', (() => {
-    idNumberSize.textContent = functions.size(idNumber.value);
-}));
+idNumber.addEventListener("change", () => {
+  idNumberSize.textContent = functions.size(idNumber.value);
+});
 
 // ********Calculator listeners
-idAddButton.addEventListener('click', (() => {
-    idResult.textContent = functions.add(Number(idCalcInput1.value), Number(idCalcInput2.value));
-}));
+idAddButton.addEventListener("click", () => {
+  idResult.textContent = functions.add(
+    Number(idCalcInput1.value),
+    Number(idCalcInput2.value)
+  );
+});
 
-idMinusButton.addEventListener('click', (() => {
-    idResult.textContent = functions.subtract(Number(idCalcInput1.value), Number(idCalcInput2.value));
-}));
+idMinusButton.addEventListener("click", () => {
+  idResult.textContent = functions.subtract(
+    Number(idCalcInput1.value),
+    Number(idCalcInput2.value)
+  );
+});
 
-idMultiButton.addEventListener('click', (() => {
-    idResult.textContent = functions.multiply(Number(idCalcInput1.value), Number(idCalcInput2.value));
-}));
+idMultiButton.addEventListener("click", () => {
+  idResult.textContent = functions.multiply(
+    Number(idCalcInput1.value),
+    Number(idCalcInput2.value)
+  );
+});
 
-idDivideButton.addEventListener('click', (() => {
-    idResult.textContent = functions.divide(Number(idCalcInput1.value), Number(idCalcInput2.value));
-}));
+idDivideButton.addEventListener("click", () => {
+  idResult.textContent = functions.divide(
+    Number(idCalcInput1.value),
+    Number(idCalcInput2.value)
+  );
+});
 
 // ********Tax calculator listeners
-idTaxButton.addEventListener('click', (() => {
-    idTaxResult.textContent = functions.taxCalculator(Number(idTaxInput.value));
-}));
+idTaxButton.addEventListener("click", () => {
+  idTaxResult.textContent = functions.taxCalculator(Number(idTaxInput.value));
+});
 
-idTaxInput.addEventListener('keypress', ((event) => {
-    if (event.keyCode === 13) {
-        idTaxResult.textContent = functions.taxCalculator(Number(idTaxInput.value));}
-}));
+idTaxInput.addEventListener("keypress", event => {
+  if (event.keyCode === 13) {
+    idTaxResult.textContent = functions.taxCalculator(Number(idTaxInput.value));
+  }
+});
+
+// ********Array listeners
+idArrayAddButton.addEventListener("click", () => {
+  idArrayMessage.textContent = functions.addToArray(Number(idArrayInput.value));
+  idArrayInput.value = "";
+});
+
+idArrayShowButton.addEventListener("click", () => {
+  idArrayMessage.textContent = functions.showArrayState();
+});
+
+idArrayTotalButton.addEventListener("click", () => {
+  idArrayMessage.textContent = functions.totalArray();
+});
+
+idArrayClearButton.addEventListener("click", () => {
+  idArrayMessage.textContent = functions.clearArray();
+});
