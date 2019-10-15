528a255075eb6c2713cddd1150b64fb0ef0fe579
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-15 17:10:52 -0600

    completed oct 15 daily

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -15,35 +15,83 @@
 // Write the function after this comment ---
 
 const functions = {
-
-    assertEquals: function (p1, p2) {
-        // console.log(p1,p2);
-        if (p1 === p2) return true
-        else if (p1 !== p2) {
-
-            console.log(`*** the two values are not the same: 
+  assertEquals: function(p1, p2) {
+    // console.log(p1,p2);
+    if (p1 === p2) return true;
+    else if (p1 !== p2) {
+      console.log(`*** the two values are not the same: 
         p1--> ${p1} 
         p2--> ${p2}`);
-
-
-        }
-        return false
-    },
-
-
-    // and before this comment ---
-
-
-    
-    // Daily Oct 9th
-    makeEmailArr: function (name) {
-        let firstName = name[0].toLowerCase();
-        let lastName = name[1].toLowerCase();
-        return `${firstName}.${lastName}@evolveu.ca`;
     }
+    return false;
+  },
 
+  // and before this comment ---
 
+  // Daily Oct 9th
+  makeEmailArr: function(name) {
+    let firstName = name[0].toLowerCase();
+    let lastName = name[1].toLowerCase();
+    return `${firstName}.${lastName}@evolveu.ca`;
+  },
 
-} // end of functions const//
+  // Daily for Oct 15
 
-export default functions
+  array1: ["Jason", "Bob", "Tom"],
+
+  // for Loop
+  myForLoop: p1 => {
+    let i;
+    let sum = 0;
+    for (i = 0; i < 5; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  },
+
+  // while loop
+  myWhileLoop: () => {
+    let txt = "";
+    let i = 0;
+    while (i < functions.array1.length) {
+      txt += functions.array1[i] + " ";
+      i++;
+    }
+    return txt;
+  },
+
+  // do while
+
+  myDoWhileLoop: () => {
+    let i = 0;
+    let sum = 0;
+    do {
+      sum = sum + i;
+      i++;
+    } while (i < 5);
+    return sum;
+  },
+
+  // for in loop
+  myForInLoop: () => {
+    let txt = "";
+    let x;
+    for (x in functions.array1) {
+      txt += functions.array1[x] + " ";
+    }
+    return txt;
+  },
+
+  // for of loop
+
+  myForOfLoop: () => {
+    let txt = "";
+    let x;
+    for (x of functions.array1) {
+      txt += x + " ";
+    }
+    return txt;
+  }
+}; // end of functions const//
+
+export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -4,27 +4,48 @@ import functions from "./daily";
 //     console.log("Hellow World");
 // });
 
-test('Check if parm are equal', () => {
-    expect(functions.assertEquals("a","b")).toBe(false);
-    expect(functions.assertEquals("a","a")).toBe(true);
-    expect(functions.assertEquals(1,2)).toBe(false);
-    expect(functions.assertEquals(2,2)).toBe(true);
-    expect(functions.assertEquals("2",2)).toBe(false);
-    expect(functions.assertEquals("This value","This value")).toBe(true);
-})
+test("Check if parm are equal", () => {
+  expect(functions.assertEquals("a", "b")).toBe(false);
+  expect(functions.assertEquals("a", "a")).toBe(true);
+  expect(functions.assertEquals(1, 2)).toBe(false);
+  expect(functions.assertEquals(2, 2)).toBe(true);
+  expect(functions.assertEquals("2", 2)).toBe(false);
+  expect(functions.assertEquals("This value", "This value")).toBe(true);
+});
 
 /*
     Write a function to format an email based on an array.
 */
 
-test('email builder from an array', () => {
-    const name = ["first", "last"];
-    expect(functions.makeEmailArr(name))
-        .toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailArr(["First", "Last"]))
-        .toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailArr(["Bill", "Smith"]))
-        .toEqual("bill.smith@evolveu.ca");
+test("email builder from an array", () => {
+  const name = ["first", "last"];
+  expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailArr(["First", "Last"])).toEqual(
+    "first.last@evolveu.ca"
+  );
+  expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual(
+    "bill.smith@evolveu.ca"
+  );
 });
 
+// test for Daily Oct 15
 
+test("test for loop", () => {
+  expect(functions.myForLoop(5)).toBe(10);
+});
+
+test("test while loop", () => {
+  expect(functions.myWhileLoop()).toBe("Jason Bob Tom ");
+});
+
+test("test do while loop", () => {
+  expect(functions.myDoWhileLoop()).toBe(10);
+});
+
+test("test for in loop", () => {
+  expect(functions.myForInLoop()).toBe("Jason Bob Tom ");
+});
+
+test("test for of loop", () => {
+  expect(functions.myForOfLoop()).toBe("Jason Bob Tom ");
+});
