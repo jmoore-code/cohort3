dc49b596a7b0799ae33fb93ca4bfcc26a572a39f
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-10-18 20:10:26 -0600

    fixed broken link for tax caclulator test when it was seperated into another file

--- src/01-getting-started/scripts/functions.test.js
+++ src/01-getting-started/scripts/functions.test.js
@@ -1,4 +1,6 @@
 import functions from "./functions";
+import taxLogic from "./taxcalc";
+
 
 test("Check the sizes", () => {
   expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
@@ -40,15 +42,15 @@ test("Does the divide function work?", () => {
 // Tax calculator tests
 
 test("Does the tax calculator work?", () => {
-  expect(functions.taxCalculator(2)).toBe(0.3);
-  expect(functions.taxCalculator(47630)).toBe(7144.5);
-  expect(functions.taxCalculator(50000)).toBe(7630.35);
-  expect(functions.taxCalculator(95259)).toBe(16908.445);
-  expect(functions.taxCalculator(100000)).toBe(18141.105);
-  expect(functions.taxCalculator(147667)).toBe(30534.525);
-  expect(functions.taxCalculator(150000)).toBe(31211.095);
-  expect(functions.taxCalculator(210371)).toBe(48718.685);
-  expect(functions.taxCalculator(250000)).toBe(61796.255);
+  expect(taxLogic.taxCalculator(2)).toBe(0.3);
+  expect(taxLogic.taxCalculator(47630)).toBe(7144.5);
+  expect(taxLogic.taxCalculator(50000)).toBe(7630.35);
+  expect(taxLogic.taxCalculator(95259)).toBe(16908.445);
+  expect(taxLogic.taxCalculator(100000)).toBe(18141.105);
+  expect(taxLogic.taxCalculator(147667)).toBe(30534.525);
+  expect(taxLogic.taxCalculator(150000)).toBe(31211.095);
+  expect(taxLogic.taxCalculator(210371)).toBe(48718.685);
+  expect(taxLogic.taxCalculator(250000)).toBe(61796.255);
 });
 
 // Working with Array tests
