301e113ab085bab53ca3f1786d97d2049fe3f942
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-11-11 20:15:58 -0700

    working on 130D, finished several methods in community class and tests

--- src/03-objects/cities/scripts/psc.js
+++ src/03-objects/cities/scripts/psc.js
@@ -1,13 +1,70 @@
 const psc = {
   city: class {
-    constructor(Name, Latitude, Longitude, Population) {
+    constructor(Key, Name, Latitude, Longitude, Population) {
+      this.key = Key;
       this.city = Name;
       this.lat = Latitude;
       this.long = Longitude;
       this.pop = Population;
     }
     show() {
-      return `${this.city} ${this.lat} ${this.long} ${this.pop}`;
+      return `Key: ${this.key}, City: ${this.city}, lat: ${this.lat}, long: ${this.long}, Population: ${this.pop}`;
+    }
+    moveIn(amount) {
+      this.pop = this.pop + amount;
+    }
+
+    moveOut(amount) {
+      this.pop = this.pop - amount;
+    }
+    howBig() {
+      if (this.pop > 100000) return "City";
+      if (this.pop > 20000) return "Large Town";
+      if (this.pop > 1000) return "Town";
+      if (this.pop > 100) return "Village";
+      if (this.pop > 0) return "Hamlet";
+      return "Zombieland";
+    }
+  },
+  community: class {
+    constructor() {
+      this.cityList = [];
+    }
+
+    createCity(Key, Name, Latitude, Longitude, Population) {
+      let message;
+      let cityInst = new psc.city(Key, Name, Latitude, Longitude, Population);
+      if (
+        this.cityList.filter(el => el.lat === Latitude && el.long === Longitude)
+          .length === 0
+      ) {
+        let cityObj = {
+          key: cityInst.key,
+          city: cityInst.city,
+          lat: cityInst.lat,
+          long: cityInst.long,
+          pop: cityInst.pop
+        };
+        this.cityList.push(cityObj);
+        message = `${Name} has been added to the database`;
+      } else {
+        message = `Error: a location already has those coordinates`;
+      }
+      return message;
+    }
+
+    whichSphere(cityInput) {
+      let filterCity = this.cityList.filter(el => el.city === cityInput);
+      if (filterCity[0].lat > 0) {
+        return "Northern Hemisphere";
+      } else {
+        return "Southern Hemisphere";
+      }
+    }
+
+    getMostNorthern() {
+        this.cityList.sort((a,b) => {return b.lat - a.lat})
+        return this.cityList[0]
     }
   }
 };
--- src/03-objects/cities/scripts/psc.test.js
+++ src/03-objects/cities/scripts/psc.test.js
@@ -1,14 +1,74 @@
 import psc from "./psc"
 
-const testCities = {
-    "1": { "key": 1, "city": "Calgary", "lat": 51.05, "long": -114.05, "pop":1500000 },
-    "2": { "key": 2, "city": "Edmonton", "lat": 53.55, "long": -113.49, "pop":800000},
-    "3": { "key": 3, "city": "Red Deer", "lat": 52.28, "long": -113.81, "pop":200000}
-}
 
 test("test show method in city class", () => {
-    let cityInst = new psc.city("Calgary", 51.05, -114.05, 1500000);
-    expect(cityInst.show()).toBe("Calgary 51.05 -114.05 1500000");
-    let cityInst2 = new psc.city("Edmonton", 53.55, -113.49, 800000);
-    expect(cityInst2.show()).toBe("Edmonton 53.55 -113.49 800000")
+    let cityInst = new psc.city(1,"Calgary", 51.05, -114.05, 1500000);
+    expect(cityInst.show()).toBe("Key: 1, City: Calgary, lat: 51.05, long: -114.05, Population: 1500000");
+    let cityInst2 = new psc.city(2, "Edmonton", 53.55, -113.49, 800000);
+    expect(cityInst2.show()).toBe("Key: 2, City: Edmonton, lat: 53.55, long: -113.49, Population: 800000")
+})
+
+test("test move in method in city class", () => {
+    let cityInst = new psc.city(1, "Calgary", 51.05, -114.05, 1500000);
+    cityInst.moveIn(10);
+    expect(cityInst.pop).toBe(1500010)
+    cityInst.moveIn(40);
+    expect(cityInst.pop).toBe(1500050)
+})
+
+test("test move out method in city class", () => {
+    let cityInst = new psc.city(1, "Calgary", 51.05, -114.05, 1500000);
+    cityInst.moveOut(10);
+    expect(cityInst.pop).toBe(1499990)
+    cityInst.moveOut(40);
+    expect(cityInst.pop).toBe(1499950)
+})
+
+test("test how big method in city class", () => {
+    let cityInst = new psc.city(1, "City size", 51.05, -114.05, 100001);
+    expect(cityInst.howBig()).toBe("City");
+    cityInst.moveOut(1)
+    expect(cityInst.howBig()).toBe("Large Town")
+    cityInst.moveOut(80000)
+    expect(cityInst.howBig()).toBe("Town")
+    cityInst.moveOut(19000)
+    expect(cityInst.howBig()).toBe("Village")
+    cityInst.moveOut(900)
+    expect(cityInst.howBig()).toBe("Hamlet")
+    cityInst.moveOut(900)
+    expect(cityInst.howBig()).toBe("Zombieland")
+})
+
+const testCities = [
+    { "key": 1, "city": "Calgary", "lat": 51.05, "long": -114.05, "pop":1500000 },
+    { "key": 2, "city": "Edmonton", "lat": 53.55, "long": -113.49, "pop":800000},
+    { "key": 3, "city": "Red Deer", "lat": 52.28, "long": -113.81, "pop":200000},
+    {"key": 4, "city": "Rio de Janeiro", "lat": -22.91, "long": -43.17, "pop": 6476631}
+]
+
+test("create city method in community class", () => {
+    let communityInst = new psc.community();
+    expect(communityInst.createCity(1, "Calgary", 51.05, -114.05, 1500000)).toBe("Calgary has been added to the database")
+    expect(communityInst.cityList[0]).toEqual({ "key": 1, "city": "Calgary", "lat": 51.05, "long": -114.05, "pop":1500000 })
+    expect(communityInst.createCity(2, "Edmonton", 53.55, -113.49, 800000)).toBe("Edmonton has been added to the database")
+    expect(communityInst.cityList[1]).toEqual({ "key": 2, "city": "Edmonton", "lat": 53.55, "long": -113.49, "pop":800000})
+    expect(communityInst.createCity(1, "Calgary", 51.05, -114.05, 1500000)).toBe("Error: a location already has those coordinates")
+})
+test("test whichSphere method in community class", () => {
+    let communityInst = new psc.community();
+    communityInst.createCity(1, "Calgary", 51.05, -114.05, 1500000)
+    communityInst.createCity(2, "Rio de Janeiro", -22.91, -43.17, 6476631)
+    expect(communityInst.whichSphere("Calgary")).toBe("Northern Hemisphere");
+    expect(communityInst.whichSphere("Rio de Janeiro")).toBe("Southern Hemisphere");
+})
+
+test("test getMostNorthern method in community class", () => {
+    let communityInst = new psc.community();
+    communityInst.createCity(1, "Calgary", 51.05, -114.05, 1500000)
+    communityInst.createCity(2, "Rio de Janeiro", -22.91, -43.17, 6476631)
+    expect(communityInst.getMostNorthern()).toEqual({ "key": 1, "city": "Calgary", "lat": 51.05, "long": -114.05, "pop":1500000 })
+    expect(communityInst.getMostNorthern()).toEqual({"key": 1, "city": "Calgary", "lat": 51.05, "long": -114.05, "pop":1500000 })
+    communityInst.createCity(3, "Edmonton", 53.55, -113.49, 800000)
+    expect(communityInst.getMostNorthern()).toEqual({ "key": 3, "city": "Edmonton", "lat": 53.55, "long": -113.49, "pop":800000})
+
 })
