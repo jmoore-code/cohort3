825308d5b914e32615becd46583e7a110ccb1dfe
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2019-11-20 16:23:54 -0700

    commented out unused variable

--- src/03-objects/cities/scripts/cities_fetch.test.js
+++ src/03-objects/cities/scripts/cities_fetch.test.js
@@ -5,12 +5,12 @@ global.fetch = require("node-fetch");
 
 const url = "http://localhost:5000/";
 
-const clients = [
-  { key: 1, city: "Calgary", lat: 51.05, long: -114.05, pop: 1500000 },
-  { key: 2, city: "Edmonton", lat: 53.55, long: -113.49, pop: 800000 },
-  { key: 3, city: "Red Deer", lat: 52.28, long: -113.81, pop: 200000 },
-  { key: 4, city: "Rio de Janeiro", lat: -22.91, long: -43.17, pop: 6000000 }
-];
+
+  // { key: 1, city: "Calgary", lat: 51.05, long: -114.05, pop: 1500000 },
+  // { key: 2, city: "Edmonton", lat: 53.55, long: -113.49, pop: 800000 },
+  // { key: 3, city: "Red Deer", lat: 52.28, long: -113.81, pop: 200000 },
+  // { key: 4, city: "Rio de Janeiro", lat: -22.91, long: -43.17, pop: 6000000 }
+
 
 test("test that add, read, delete data works", async () => {
   // clear server of data
