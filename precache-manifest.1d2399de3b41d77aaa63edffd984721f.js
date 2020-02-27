581b36d83ef15f3ae9ed3a0ab78ea362500ba0d3
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2020-02-27 14:44:38 -0700

    Updates

---
+++ precache-manifest.1d2399de3b41d77aaa63edffd984721f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edf545a7ff178d600e2fa9531145ac2",
+    "url": "/cohort3/index.html"
+  },
+  {
+    "revision": "8eafefccc4978283cb34",
+    "url": "/cohort3/static/css/main.b4d8ca61.chunk.css"
+  },
+  {
+    "revision": "d4af50c514e4bad4f3e3",
+    "url": "/cohort3/static/js/2.69bc9d38.chunk.js"
+  },
+  {
+    "revision": "dc96b2fe57711157dbbe5749faf088ee",
+    "url": "/cohort3/static/js/2.69bc9d38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8eafefccc4978283cb34",
+    "url": "/cohort3/static/js/main.11ef31dc.chunk.js"
+  },
+  {
+    "revision": "ebe1b5413e834512cb02",
+    "url": "/cohort3/static/js/runtime-main.133e12dd.js"
+  },
+  {
+    "revision": "bbe80169f03234763393a73fc300dec2",
+    "url": "/cohort3/static/media/cityscape.bbe80169.svg"
+  },
+  {
+    "revision": "a1cfdc15c94d3f40ee6857d46cf12ed2",
+    "url": "/cohort3/static/media/database.a1cfdc15.svg"
+  },
+  {
+    "revision": "bb69a98b0e722a99fa6fa80c17e528a1",
+    "url": "/cohort3/static/media/gear.bb69a98b.svg"
+  },
+  {
+    "revision": "8eb8dfd610cd147b9752fa7dd191f36f",
+    "url": "/cohort3/static/media/home.8eb8dfd6.svg"
+  },
+  {
+    "revision": "c8ff41bb6186cee34cc7706155a5d6e9",
+    "url": "/cohort3/static/media/link.c8ff41bb.svg"
+  },
+  {
+    "revision": "daad0adf699bf1eb4fec343469f34dfa",
+    "url": "/cohort3/static/media/piggy-bank.daad0adf.svg"
+  },
+  {
+    "revision": "2ce701bde15d18fddd028b0173eb4d4b",
+    "url": "/cohort3/static/media/react-brands.2ce701bd.svg"
+  },
+  {
+    "revision": "3622b62f2d2273ae0c056e001d872179",
+    "url": "/cohort3/static/media/tic-tac-toe.3622b62f.svg"
+  }
+]);
